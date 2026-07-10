95d300a17970a76fb262bd6ac50bb9fb89cf8cee
VinnyIadarola <viadarola@outlook.com>
2026-07-10 17:57:31 -0400

    updating example and exit protocol to be more clear

--- example.sv
+++ example.sv
@@ -49,11 +49,38 @@ module example;
         comm.add_out("c", sig_ref #(1)::make(c_if));
 
         cycle = 0;
-        forever begin
-            comm.grabCommVals(cycle, delete_read_file);
-            comm.writeCommVals(cycle);
-            cycle++;
-            #5;
-        end
+
+        comm.grabCommVals(cycle, delete_read_file);
+        comm.writeCommVals(cycle);
+        cycle++;
+
+
+        #10 // wait will IO generates next cycles txt file (since its example we just wait but function polls)
+        comm.grabCommVals(cycle, delete_read_file);
+        #10; // Allow results to propagate (replace time units with repeat @(posedge clk) or what not) 
+        comm.writeCommVals(cycle);
+        cycle++;
+
+
+        fork : POLLING_EXAMPLE 
+            begin 
+                comm.grabCommVals(cycle, delete_read_file);
+            end
+            begin
+                // showing that we will just wait till next text file is seen which wont happen as we are missing cyc_2
+                #50; 
+            end
+        join_any
+        disable POLLING_EXAMPLE;
+
+        cycle++;
+
+        // IO dictates end of communication through
+        #10; 
+        comm.grabCommVals(cycle, delete_read_file);
+
+
+
+
     end
 endmodule
